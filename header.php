f33a55c474ee4fe0ce6d312508e4408c15010a8a
Filcis <cislakfilip@gmail.com>
2015-08-19 23:16:39 +0200

    updated slider

--- header.php
+++ header.php
@@ -58,6 +58,15 @@
 <?php if ( is_front_page() && !is_home() ) : ?>	
 <div class="slider-wrapper">
 <?php echo l_slider_function(); ?>
+	
+<div id="logo-wrapper"><img class="logo" src="<?php echo get_stylesheet_directory_uri();?>/logo.svg"></div>
+<div class="page-title"><h1 class="entry-title">Hala Lipowska Schronisko PTTK</h1>
+<h3 class="read-more">Czas na relaks</h3>
+<img class="arrow-down-button-secondary" src="<?php echo get_stylesheet_directory_uri();?>/ikony/arrow-down-secondary.svg">
+</div>
+
+
+	
 </div>	
 <?php else : ?>
 <!-- get futured image instead slider -->
