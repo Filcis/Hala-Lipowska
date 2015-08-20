063dabdacd14c355d50938ae23e39bdaa14bdb70
Filcis <cislakfilip@gmail.com>
2015-08-20 12:43:28 +0200

    minor changes
    Removed obsolete jquery function, tidied code in header (moved parts to
    external templates). Added gradient to darken lower part of slider.

---
+++ header-home.php
@@ -0,0 +1,9 @@
+<div class="slider-wrapper image-wrapper-color">
+<?php echo l_slider_function(); ?>
+	
+<div id="logo-wrapper"><img class="logo" src="<?php echo get_stylesheet_directory_uri();?>/logo.svg"></div>
+<div class="page-title"><h1 class="entry-title">Hala Lipowska Schronisko PTTK</h1>
+<h3 class="read-more">Czas na relaks</h3>
+<img class="arrow-down-button-secondary" src="<?php echo get_stylesheet_directory_uri();?>/ikony/arrow-down-secondary.svg">
+</div>
+</div>	
--- js/hl-slider.js
+++ js/hl-slider.js
@@ -30,14 +30,6 @@ $(window).load(function(){
 		},7000);
 })
 
-$(document).ready(function(){
-//    var a = $(window).height() - $('#menu').height();
-    var a = $(window).height();
-//    $(".slider-wrapper").css('height', a);
-//	$("#image-wrapper").css('height', a);
 
-});
-
-	
 });
 
