efaf49ac530b066afca8ba6b298eb1ef4b23babb
Filcis <cislakfilip@gmail.com>
2015-08-20 19:33:49 +0200

    added conditionals for mobile in header

--- header-home.php
+++ header-home.php
@@ -9,5 +9,14 @@
 </div>
 </div>	
 
-<?php else : ?>
+<?php else : 
+global $post;
+$hl_src_mobile = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array(400,600), false, '' ); ?>
+<div id="image-wrapper" style="background-image: url(<?php echo $hl_src_mobile[0]; ?> );">
+<div class="image-wrapper-color"></div>
+<div class="page-title"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+<h3 class="read-more">czas na relaks</h3>
+<img class="arrow-down-button-secondary" src="<?php echo get_stylesheet_directory_uri();?>/ikony/arrow-down-secondary.svg">
+</div>
+</div>
 <?php endif; ?>
