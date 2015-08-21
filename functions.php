0a46dd5da9fc39d26bacc415808b7202db6e9229
Filcis <cislakfilip@gmail.com>
2015-08-21 15:11:57 +0200

    deleted custom menu plugin

--- functions.php
+++ functions.php
@@ -42,7 +42,7 @@ function hl_setup() {
 	 */
 	add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
+	// MENUS
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary Menu', 'hl' ),
 	) );
